14bc7e249e015f64a4daf9e4d5ba11fe9083125d
Torres Sue <109887704+vzep@users.noreply.github.com>
2025-01-07 19:25:15 +0800

    Update sidebar-tool.tsx

--- components/sidebar/sidebar-tool.tsx
+++ components/sidebar/sidebar-tool.tsx
@@ -14,7 +14,7 @@ import dayjs from 'dayjs';
 import PortalState from 'libs/web/state/portal';
 import useI18n from 'libs/web/hooks/use-i18n';
 import HeadwayWidget from '@notea/headway-widget';
-import useMounted from 'libs/web/hooks/use-mounted';
+// import useMounted from 'libs/web/hooks/use-mounted';
 import { useRouter } from 'next/router';
 
 const ButtonItem = forwardRef<HTMLDivElement, HTMLProps<HTMLDivElement>>(
