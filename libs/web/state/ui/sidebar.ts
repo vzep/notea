12f85097098f9662e1fd9a0de5044e00f434e200
Torres Sue <109887704+vzep@users.noreply.github.com>
2025-01-07 16:55:17 +0800

    Update sidebar.ts

--- libs/web/state/ui/sidebar.ts
+++ libs/web/state/ui/sidebar.ts
@@ -2,47 +2,36 @@ import useSettingsAPI from 'libs/web/api/settings';
 import { isBoolean } from 'lodash';
 import { useState, useCallback } from 'react';
 
-export default function useSidebar(initState = false, isMobileOnly = false) {
+export default function useSidebar(initState = true, isMobileOnly = false) {
     const [isFold, setIsFold] = useState(initState);
-    const [isPinned, setIsPinned] = useState(false);
     const { mutate } = useSettingsAPI();
 
     const toggle = useCallback(
         async (state?: boolean) => {
             setIsFold((prev) => {
-                const nextFold = isBoolean(state) ? state : !prev;
+                const isFold = isBoolean(state) ? state : !prev;
+
                 if (!isMobileOnly) {
                     mutate({
-                        sidebar_is_fold: nextFold,
+                        sidebar_is_fold: isFold,
                     });
                 }
-                return nextFold;
+
+                return isFold;
             });
         },
         [isMobileOnly, mutate]
     );
 
     const open = useCallback(() => {
-        toggle(false).catch((v) =>
-            console.error('Error whilst opening sidebar: %O', v)
-        );
+        toggle(true)
+            ?.catch((v) => console.error('Error whilst opening sidebar: %O', v));
     }, [toggle]);
 
     const close = useCallback(() => {
-        toggle(true).catch((v) =>
-            console.error('Error whilst closing sidebar: %O', v)
-        );
+        toggle(false)
+            ?.catch((v) => console.error('Error whilst closing sidebar: %O', v));
     }, [toggle]);
 
-    // 新增 pinned 切换，假设用户选了 pin 也可以做一次 mutate 存储
-    const togglePin = useCallback(() => {
-        setIsPinned((prev) => {
-            const nextState = !prev;
-            // 也可以做和 sidebar_is_fold 同样的存储逻辑
-            // mutate({ sidebar_is_pinned: nextState });
-            return nextState;
-        });
-    }, []);
-    
-    return { isFold, toggle, open, close, isPinned, togglePin };
+    return { isFold, toggle, open, close };
 }
