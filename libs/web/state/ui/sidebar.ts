e40694bb761ccbb9fd23900365e022c80e456048
Torres Sue <109887704+vzep@users.noreply.github.com>
2025-01-07 22:22:18 +0800

    Update sidebar.ts

--- libs/web/state/ui/sidebar.ts
+++ libs/web/state/ui/sidebar.ts
@@ -4,40 +4,34 @@ import { useState, useCallback } from 'react';
 
 export default function useSidebar(initState = false, isMobileOnly = false) {
     const [isFold, setIsFold] = useState(initState);
-    const [isHovered, setIsHovered] = useState(false);
     const { mutate } = useSettingsAPI();
 
     const toggle = useCallback(
         async (state?: boolean) => {
             setIsFold((prev) => {
-                const nextIsFold = isBoolean(state) ? state : !prev;
-                
+                const isFold = isBoolean(state) ? state : !prev;
+
                 if (!isMobileOnly) {
                     mutate({
-                        sidebar_is_fold: nextIsFold,
+                        sidebar_is_fold: isFold,
                     });
                 }
-                
-                return nextIsFold;
+
+                return isFold;
             });
-            setIsHovered(false);
         },
         [isMobileOnly, mutate]
     );
 
-    const setHovered = useCallback((state: boolean) => {
-        if (isFold) {
-            setIsHovered(state);
-        }
-    }, [isFold]);
-
     const open = useCallback(() => {
-        toggle(false)?.catch((v) => console.error('Error whilst opening sidebar: %O', v));
+        toggle(true)
+            ?.catch((v) => console.error('Error whilst opening sidebar: %O', v));
     }, [toggle]);
 
     const close = useCallback(() => {
-        toggle(true)?.catch((v) => console.error('Error whilst closing sidebar: %O', v));
+        toggle(false)
+            ?.catch((v) => console.error('Error whilst closing sidebar: %O', v));
     }, [toggle]);
 
-    return { isFold, isHovered, setHovered, toggle, open, close };
+    return { isFold, toggle, open, close };
 }
