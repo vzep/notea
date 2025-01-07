e9fece0347ef8ac0f049567e487d9f606b86440c
Torres Sue <109887704+vzep@users.noreply.github.com>
2025-01-07 19:20:16 +0800

    Update sidebar.ts

--- libs/web/state/ui/sidebar.ts
+++ libs/web/state/ui/sidebar.ts
@@ -2,8 +2,10 @@ import useSettingsAPI from 'libs/web/api/settings';
 import { isBoolean } from 'lodash';
 import { useState, useCallback } from 'react';
 
-export default function useSidebar(initState = true, isMobileOnly = false) {  // 设置初始状态为折叠
+
+export default function useSidebar(initState = false, isMobileOnly = false) {  // 默认为折叠状态
     const [isFold, setIsFold] = useState(initState);
+    const [isHovered, setIsHovered] = useState(false); // 新增悬停状态
     const { mutate } = useSettingsAPI();
 
     const toggle = useCallback(
@@ -23,6 +25,10 @@ export default function useSidebar(initState = true, isMobileOnly = false) {  //
         [isMobileOnly, mutate]
     );
 
+    const setHovered = useCallback((state: boolean) => {
+        setIsHovered(state);
+    }, []);
+
     const open = useCallback(() => {
         toggle(false)?.catch((v) => console.error('Error whilst opening sidebar: %O', v));
     }, [toggle]);
@@ -31,5 +37,5 @@ export default function useSidebar(initState = true, isMobileOnly = false) {  //
         toggle(true)?.catch((v) => console.error('Error whilst closing sidebar: %O', v));
     }, [toggle]);
 
-    return { isFold, toggle, open, close };
+    return { isFold, isHovered, setHovered, toggle, open, close };
 }
