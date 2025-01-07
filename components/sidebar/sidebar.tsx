3af8bf2fd366dde871cc90907efd2d6e2755d1de
Torres Sue <109887704+vzep@users.noreply.github.com>
2025-01-07 16:43:04 +0800

    Update sidebar.tsx

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -22,15 +22,36 @@ const BrowserSidebar: FC = () => {
         split: { sizes },
     } = UIState.useContainer();
 
+    const { isFold, isPinned, open, close } = sidebar;
+
     return (
+        // 当 sidebar 折叠且未 pin 时，默认使用 -translate-x-full 隐藏，
+        // 悬停（鼠标移入）后执行 open() 展开
+        // 如果 pinned，则一直展开
         <section
-            className="flex h-full fixed left-0"
+            className={`
+                flex h-full fixed left-0
+                transition-transform duration-300
+                ${isFold && !isPinned ? '-translate-x-full' : 'translate-x-0'}
+            `}
             style={{
                 width: `calc(${sizes[0]}% - 5px)`,
             }}
+            onMouseEnter={() => {
+                // 如果没被 pin，鼠标进来就自动展开
+                if (!isPinned && isFold) {
+                    open();
+                }
+            }}
+            onMouseLeave={() => {
+                // 如果没被 pin，鼠标离开就自动折叠
+                if (!isPinned && !isFold) {
+                    close();
+                }
+            }}
         >
             <SidebarTool />
-            {sidebar.isFold ? null : <SideBarList />}
+            {isFold ? null : <SideBarList />}
         </section>
     );
 };
