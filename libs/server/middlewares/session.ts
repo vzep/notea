f52644bde53372bc3b1f8a37b35da1cb48d6c78f
Torres Sue <109887704+vzep@users.noreply.github.com>
2025-01-07 13:37:51 +0800

    Update session.ts

--- libs/server/middlewares/session.ts
+++ libs/server/middlewares/session.ts
@@ -5,7 +5,7 @@ import { Middleware, NextHandler } from 'next-connect';
 
 const sessionOptions = () => ({
     cookieName: 'notea-auth',
-    password: md5('notea' + (config().auth as BasicAuthConfiguration).password), // NOTE(tecc): in the future, if this field becomes null, it will be an issue
+    password: md5('noteax' + (config().auth as BasicAuthConfiguration).username + (config().auth as BasicAuthConfiguration).password), // NOTE(tecc): in the future, if this field becomes null, it will be an issue
     // if your localhost is served on http:// then disable the secure flag
     cookieOptions: {
         secure: config().server.useSecureCookies,
