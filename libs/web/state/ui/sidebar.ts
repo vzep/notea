29d0e9e5a017be09f55b0221c759970cfbc6f0e8
Torres Sue <109887704+vzep@users.noreply.github.com>
2025-01-07 17:15:06 +0800

    Update sidebar.ts

--- libs/web/state/ui/sidebar.ts
+++ libs/web/state/ui/sidebar.ts
@@ -2,22 +2,22 @@ import useSettingsAPI from 'libs/web/api/settings';
 import { isBoolean } from 'lodash';
 import { useState, useCallback } from 'react';
 
-export default function useSidebar(initState = true, isMobileOnly = false) {  // 默认设置为折叠状态
+export default function useSidebar(initState = true, isMobileOnly = false) {  // 设置初始状态为折叠
     const [isFold, setIsFold] = useState(initState);
     const { mutate } = useSettingsAPI();
 
     const toggle = useCallback(
         async (state?: boolean) => {
             setIsFold((prev) => {
-                const isFold = isBoolean(state) ? state : !prev;
+                const nextIsFold = isBoolean(state) ? state : !prev;
 
                 if (!isMobileOnly) {
                     mutate({
-                        sidebar_is_fold: isFold,
+                        sidebar_is_fold: nextIsFold,
                     });
                 }
 
-                return isFold;
+                return nextIsFold;
             });
         },
         [isMobileOnly, mutate]
